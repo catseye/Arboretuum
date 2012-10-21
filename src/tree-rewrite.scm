b170343f8183ed2d469bf3e598a21b9fc7a26f0a
catseye <cpressey@gmail.com>
2012-10-21 18:02:58 +0100

    Place in public domain.  Convert CRLF -> LF in README.

--- src/tree-rewrite.scm
+++ src/tree-rewrite.scm
@@ -2,35 +2,9 @@
 ; Bottom-Up Tree-Rewriting (Term-Rewriting)
 ; Chris Pressey, sometime late January 2006
 ;
-
-; Copyright (c)2008 Cat's Eye Technologies.  All rights reserved.
+; This work is in the public domain.  See the file UNLICENSE for more
+; information.
 ;
-; Redistribution and use in source and binary forms, with or without
-; modification, are permitted provided that the following conditions
-; are met:
-;
-; 1. Redistributions of source code must retain the above copyright
-;    notices, this list of conditions and the following disclaimer.
-; 2. Redistributions in binary form must reproduce the above copyright
-;    notices, this list of conditions, and the following disclaimer in
-;    the documentation and/or other materials provided with the
-;    distribution.
-; 3. Neither the names of the copyright holders nor the names of their
-;    contributors may be used to endorse or promote products derived
-;    from this software without specific prior written permission. 
-;
-; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-; ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, BUT NOT
-; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-; FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
-; COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-; ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-; POSSIBILITY OF SUCH DAMAGE.
 
 ;
 ; Try unifying the pattern part of the given rule with the given term;
