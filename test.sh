7ceaeaf1aceea6d85f7751fb2718542248f8c9c0
Cat's Eye Technologies <cpressey@catseye.tc>
2014-04-22 14:21:41 +0100

    Add test driver.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd src
+csi -q -b tests.scm > test-output.txt || exit 1
+#huski tests.scm > test-output.txt || exit 1
+#plt-r5rs tests.scm > test-output.txt || exit 1
+cat test-output.txt
+if grep -q FAILED test-output.txt; then
+    echo "FAILED"
+    rm test-output.txt
+    exit 1
+else
+    echo "PASSED"
+    rm test-output.txt
+    exit 0
+fi
